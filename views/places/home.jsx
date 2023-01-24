d3caca7b98d8181a4df95fda1f02e5d3b370346c
Ben Yoon <BYoon12@gmail.com>
2023-01-24 10:32:31 -0500

    Adding styles to REST-Rant

--- views/places/home.jsx
+++ views/places/home.jsx
@@ -1,6 +1,10 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {}
+function home () {
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+}
 
 module.exports = home
